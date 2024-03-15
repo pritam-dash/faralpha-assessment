2b04e4a30910d0ebc25218284fab43726cae917e
Pritam Dash <pritamdash84@gmail.com>
2024-03-16 01:34:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
 const app = express();
-const PORT = 80;
+const APP_PORT = 80;
 
 app.get('/sayHello', (req, res) => {
   res.json({ message: 'Hello User' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${APP_PORT}`);
 });
